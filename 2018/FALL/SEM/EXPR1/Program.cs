9d1c6e86424a5fd71a61b092fd359c74bc3cf41a
Dima Cheg <cheg15032001@gmail.com>
2018-09-12 13:40:22 +0300

    [add] expr1

---
+++ 2018/FALL/SEM/EXPR1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Expr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = temp;
+            // без добавления новой переменной возможны ошибки
+        }
+    }
+}
