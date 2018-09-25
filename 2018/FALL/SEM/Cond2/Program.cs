b122225e53852b5c7d1f7fd4d9418170a132722f
Dima Cheg <cheg15032001@gmail.com>
2018-09-25 23:00:26 +0300

    [add] cond2

---
+++ 2018/FALL/SEM/Cond2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cond2
+{
+    /* автор: Чегодаев Дима
+     * группа 11-808
+     * семинарская задача */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите размеры предметов");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (Math.Max(Math.Min(x, y), Math.Min(z, y)) <= Math.Max(a, b) &&
+                Math.Min(Math.Min(x, y), z) <= Math.Min(a, b)) 
+            {
+                // если среднее измерение бруса меньше максимального измерения отверстия
+                // и минимальное измерение бруса меньше минимального измерения отверстия
+                Console.WriteLine("пролезет");
+            }
+            else
+            {
+                Console.WriteLine("не пролезет");
+            }
+        }
+    }
+}
