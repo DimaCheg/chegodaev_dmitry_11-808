be309a9590c3c2d302d31b37d5c84a58af7bad07
Dima Cheg <cheg15032001@gmail.com>
2018-09-25 23:18:38 +0300

    [add] cond4

---
+++ 2018/FALL/SEM/Cond4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cond4
+{
+    /* автор: Чегодаев Дима
+     * группа 11-808
+     * семинарская задача */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите А: ");
+            double A = double.Parse(Console.ReadLine());
+            Console.Write("введите B: ");
+            double B = double.Parse(Console.ReadLine());
+            Console.Write("введите C: ");
+            double C = double.Parse(Console.ReadLine());
+            Console.Write("введите D: ");
+            double D = double.Parse(Console.ReadLine());
+            // за начало перечесения берем начало того отрезка,
+            // чья координата больше
+            double left = Math.Max(A, C);
+            // за конец пересечения берем конец того отрезка,
+            // чья координата меньше
+            double right = Math.Min(B, D);
+            // если конец отрезка пересечения лежит 
+            // левее, чем начало, то отрезки не пересекаются
+            Console.WriteLine(Math.Max(right - left, 0));
+        }
+    }
+}
