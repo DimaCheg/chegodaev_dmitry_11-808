220c2275efc425f959c0b02ea5cf2468100fec5f
dima_cheg <cheg15032001@gmail.com>
2019-03-20 22:52:37 +0300

    [add] ulearn LD4

---
+++ 2019/informatics/seminar tasks/BaconCipher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LDsem
+{
+    class BaconCipher
+    {
+        HashSet<string> set = new HashSet<string>();
+        public int GetSubstringCount(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                var substring = new StringBuilder();
+                for (int j = i; j < text.Length; j++)
+                {
+                    substring.Append(text[j]);
+                    set.Add(substring.ToString());
+                }
+            }
+            return set.Count;
+        }
+    }
+}
