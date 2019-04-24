48cd79446c9dd3623b6dfa620882eb6661db7fe0
dima_cheg <cheg15032001@gmail.com>
2019-04-24 14:38:07 +0300

    [add] sem2
    [add] sem2

---
+++ 2019/Algorithms and Data Systems/Sem2/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SemTask2
+{
+    class Generator
+    {
+        public List<Point> GetPoints(int count)
+        {
+            var rand = new Random();
+            var list = new List<Point>();
+            for (int i=0;i<count; i++)
+            {
+                list.Add(new Point(rand.Next(-100, 100), rand.Next(-100, 100)));
+            }
+            return list;
+        }
+    }
+}
