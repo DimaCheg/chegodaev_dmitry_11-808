b5823058ab6331a1a898dcca0de5f65c7245cbec
dima_cheg <cheg15032001@gmail.com>
2019-05-14 13:35:57 +0300

    [add] task2

---
+++ 2019/MISC/ControlTest2/Task2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ControlTest2
+{
+    class Task2
+    {
+        public void GetAnswer(string path)
+        {
+            string json;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                json = sr.ReadToEnd();
+            }
+            Letter[] let = JsonConvert.DeserializeObject<Letter[]>(json);
+            for (int i=0; i< let.Length; i += 2)
+            {
+                Console.WriteLine($"комментарий {i}: " + let[i].body.Length + "символов");
+            }
+        }        
+    }
+
+    class Letter
+    {
+        public int postId;
+        public int id;
+        public string email;
+        public string body;
+    }
+}
