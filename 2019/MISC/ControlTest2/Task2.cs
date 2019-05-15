d7556a8a64511aca71d824eb0fc685092599d891
dima_cheg <cheg15032001@gmail.com>
2019-05-15 11:19:47 +0300

    [add] task2

--- 2019/MISC/ControlTest2/Task2.cs
+++ 2019/MISC/ControlTest2/Task2.cs
@@ -1,15 +1,25 @@
-﻿using System;
+using System;
 using System.IO;
 using Newtonsoft.Json;
+using System.Linq;
+using System.Threading;
 
 namespace ControlTest2
 {
     class Task2
     {
+        public string Path { get; private set; } 
         public void GetAnswer(string path)
         {
+            Path = path;
+            Thread thread = new Thread(new ThreadStart(Read));
+            thread.Start();
+        }
+
+        private void Read()
+        {
             string json;
-            using (StreamReader sr = new StreamReader(path))
+            using (StreamReader sr = new StreamReader(Path))
             {
                 json = sr.ReadToEnd();
             }
@@ -17,7 +27,7 @@ namespace ControlTest2
             for (int i = 1; i < let.Length; i += 2)
             {
                 var count = let[i].body.Where(c => char.IsLetter(c)).Count();
-                Console.WriteLine($"комментарий {i + 1}: " + count + "букв");
+                Console.WriteLine($"комментарий {i + 1}: " + count + " букв");
             }
         }        
     }
